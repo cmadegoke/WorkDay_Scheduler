a293d4307c2c9b81f56426d1ef405fdef26ea6e4
Catherine Adegoke <cmadegoke@gmail.com>
2020-01-06 21:53:05 -0500

    layout creeated

--- style.js
+++ style.js
@@ -1,20 +1,11 @@
 var timeBlock = document.querySelector('.time-block');
-
 var saveBtn = document.querySelector('.saveBtn');
-
 var textArea= document.querySelector('.textarea');
 
 
-
-
-
-
-
 function storedata(key, data){
-    localStorage.setItem (key,data);
-}
-
-
+    localStorage.setItem (key, data);
+};
 
 function saveBtnHandler(){
     console.log ('save button pressed');
@@ -23,10 +14,21 @@ function saveBtnHandler(){
 
 saveBtn.addEventListener('click', function(event) {
     event.preventDefault();  
-    const data = event.target.previousElementSibling.children[0].value
-    const time = event.target.previousElementSibling.previousElementSibling.children[0].innerHTML
-    storedata(time, data)
-    // console.log(event, data)
+    // var data = event.target.previousElementSibling.children[0].value
+
+    // var data = event.target.previousElementSibling.children[1].value
+    // var time = event.target.previousElementSibling.previousElementSibling.children[0].innerHTML
+    // var time = event.target.previousElementSibling.previousElementSibling.children[1].innerHTML
+    // storedata(time, data)
+    console.log(event)
 });
 
-document.getElementById("12am").value = localStorage.getItem("12am")
+document.getElementById("9am").value = localStorage.getItem("9am");
+document.getElementById("10am").value = localStorage.getItem("10am");
+document.getElementById("11am").value = localStorage.getItem("11am");
+document.getElementById("12pm").value = localStorage.getItem("12pm");
+document.getElementById("1pm").value = localStorage.getItem("1pm");
+document.getElementById("2pm").value = localStorage.getItem("2pm");
+document.getElementById("3pm").value = localStorage.getItem("3pm");
+document.getElementById("4pm").value = localStorage.getItem("4pm");
+document.getElementById("5pm").value = localStorage.getItem("5pm");
